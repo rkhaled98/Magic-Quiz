74c0e0eec4e211c5b2e9e6ae6797a53b06bb54d7
Rafi Khaled <rkhaled@vassar.edu>
2019-04-10 11:09:52 -0400

    cleaning code

--- main.js
+++ main.js
@@ -5,22 +5,18 @@ $('#submit').on('click', function (e) {
     }).toArray();
 
     $.getJSON("data.json", function (data) {
-        // console.log(data);
         let num_qs = data.questions.length;
         modal.style.display = "block";
         if (choices.length != num_qs) {
             modal.style.display = "block";
         }
         else {
-            // console.log(choices[0]);
 
             freqs = {};
             curr_max_val = 0;
             curr_max_choice = choices[0];
-            // console.log(curr_max_choice);
             for (var choice in choices) {
                 choice = choices[choice]
-                // console.log(choices);
                 if (freqs[choice] === undefined) {
                     freqs[choice] = 1;
                     if (1 > curr_max_val) {
@@ -36,11 +32,8 @@ $('#submit').on('click', function (e) {
                     }
                 }
             }
-            // console.log(curr_max_choice);
 
             $.getJSON("data.json", function (data) {
-                // console.log(data.outcomes);
-                // console.log(data.outcomes[curr_max_choice[0]]);
                 let el = data.outcomes[curr_max_choice[0]];
                 $('#result-question').text(`${data.title}`);
                 $('#result-you-got').text(`You got: ${el.text}`);
@@ -51,97 +44,46 @@ $('#submit').on('click', function (e) {
 
         }
     });
-
-
-
-    // now you have an array of choices = ["valueofradiobox1", "valueofradiobox2", "valueofradiobox2"]
-    // you'll need to do some calculations with this
-    // a naive approach would be to just choose the most common option - seems reasonable
 });
 
-// Get the modal
+// below modal block from w3schools
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("submit");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
 btn.onclick = function () {
-    // console.log(this);
     modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
     modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+// end w3schools credit
 
-$("div").click( () =>{
-    // console.log(this);
-});
 
-$('input').change(()=>{
-    // console.log(this);
-})
-
-$("input").on("change",function() {
-    alert(this.value);
-});
-
+// DANGER: BAD CODE
+// listen for clicks on the document and grey out non selected boxes
 $(document).on("click", function (event) {
-    // console.log(event.target.parentNode.parentNode.parentNode);
-    
-    $(event.target.parentNode.parentNode.parentNode).children().each(function(){
-        if ($($(this)[0].children[0].children[0])[0] === undefined){
-            
+    $(event.target.parentNode.parentNode.parentNode).children().each(function () {
+        if ($($(this)[0].children[0].children[0])[0] === undefined) {
+
         }
-        else{
-            if(!$($(this)[0].children[0].children[0])[0].checked ){
+        else {
+            if (!$($(this)[0].children[0].children[0])[0].checked) {
                 $($(this)[0].children[0].children[1]).css("opacity", "0.3");
             }
-        // console.log($($($($(this)[0].children[0]))[0].children[1]).css("opacity", "1.0"));
-        // console.log($($($($(this)[0].children[0]))[0].children[1]))
         }
     })
-
-    console.log($($(event.target.parentNode)[0].children[1]).css("opacity", "1.0"));
-
-    // todo: simply need to make the clicked element higher opacity, go to event.target.parentNode.img
-    // console.log(event.target.parentNode.parentNode.parentNode);
 });
 
-// $('img').on("click", function(event){
-//     console.log(event.target);
-// })
 
-// $('input').on("click", function(event){
-//     console.log(event.target);
-// })
-
-// $(document).on("click", function () {
-//     var clickedBtnID = $(this); // or var clickedBtnID = this.id
-//     console.log(clickedBtnID);
-//  });
-
-
+// build the quiz items
 $.getJSON("data.json", function (data) {
     $('#quiz_title').text(`${data.title}`);
     data.questions.forEach((question, i) => {
-        // $('.quiz').append(
-        //     `
-        //     <div id= "${question.question_id}" class="question">
-        //     <h2 class="question-header">${question.question_name}</h2>
-        //     `);
         $(`
             <div id= "${question.question_id}" class="question">
             <div class="question-header-div">
@@ -161,7 +103,7 @@ $.getJSON("data.json", function (data) {
                 </div> `
                 );
             }
-            else{
+            else {
                 $(`#${question.question_id}`).append(
                     `
                 <div class="question-item">
@@ -170,15 +112,12 @@ $.getJSON("data.json", function (data) {
                         <h1 class="question-text-choice"> ${answer.text} </h1>
                     </label>
                 </div> `
-                ); 
+                );
             }
 
         });
         $(`</div>`).insertBefore('#submit');
         $(`#${question.question_id} > .question-header-div`).css("background-image", `url(${question.question_img_url})`)
     })
-    // now you can do something with this data. 
-    // remember you can only work with the data in this callback
-    // data.title has the title
-    // maybe you want to loop through data.questions? 
+
 });
